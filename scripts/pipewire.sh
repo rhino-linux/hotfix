cbb85b1e395b3afac5688b5ae89cab408ad128aa
ook37 <oren+12345@taumoda.com>
2025-01-24 01:16:25 -0500

    blooblah

--- scripts/pipewire.sh
+++ scripts/pipewire.sh
@@ -1,32 +1,31 @@
 #!/usr/bin/env bash
 
-mkdir -p /tmp/audiofix && cd /tmp/audiofix
-deps=(
-  "libcanberra-pulse"
-  "libconfig++9v5"
-  "libffado2"
-  "libglibmm-2.4-1t64"
-  "libxml++2.6-2v5"
-  "libpipewire-0.3-common"
-  "pipewire-audio"
-)
-pacdeps=(
-  "libwebrtc-audio-processing1-deb"
-  "libspa-0.2-modules-deb"
-  "libspa-0.2-bluetooth-deb"
-  "libpipewire-0.3-0t64-deb"
-  "libpipewire-0.3-modules-deb"
-  "libpipewire-0.3-modules-x11-deb"
-  "pipewire-bin-deb"
-  "pipewire-deb"
-  "pipewire-alsa-deb"
-  "pipewire-jack-deb"
-  "pipewire-pulse-deb"
-  "pipewire-v4l2-deb"
-  "gstreamer1.0-pipewire-deb"
-)
-sudo apt update && sudo apt install "${deps[@]}" -y
-echo N | for i in "${pacdeps[@]}"; do pacstall -QaB "${i}"#6926; done
-sudo dpkg -i *.deb
-cd ~/
-sudo rm -r /tmp/audiofix
+hotfix() {
+  local name pkgver base_url packages pacdeps
+  name="pipewire-bundle"
+  pkgver="1.2.7-1+b1"
+  base_url="http://ftp.debian.org/debian/pool/main/p/pipewire"
+  packages=(
+    "libspa-0.2-modules"
+    "libspa-0.2-bluetooth"
+    "libpipewire-0.3-0t64"
+    "libpipewire-0.3-modules"
+    "libpipewire-0.3-modules-x11"
+    "pipewire-bin"
+    "pipewire"
+    "pipewire-alsa"
+    "pipewire-jack"
+    "pipewire-pulse"
+    "pipewire-v4l2"
+    "gstreamer1.0-pipewire"
+  )
+  pacdeps=("libwebrtc-audio-processing1-deb")
+
+  pacstall -PI "${pacdeps[@]}"
+
+  for i in "${!packages[@]}"; do
+    wget -q "${base_url}/${packages[i]}_${pkgver}_${CARCH}.deb" -O "${i}_${name}.deb"
+  done
+
+  sudo apt-get install --allow-downgrades -f ./[0-9]*"${name}".deb -y
+}
