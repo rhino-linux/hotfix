e4e99d8f0d8061b7c164fb3b24d6dd685a14e15c
ook37 <oren+12345@taumoda.com>
2025-02-11 17:55:58 -0500

    fix(pipewire): bump versions

--- scripts/pipewire.sh
+++ scripts/pipewire.sh
@@ -3,7 +3,7 @@
 hotfix() {
   local name pkgver base_url packages pacdeps
   name="pipewire-bundle"
-  pkgver="1.2.7-1+b1"
+  pkgver="1.2.7-1+b2"
   base_url="http://ftp.debian.org/debian/pool/main/p/pipewire"
   packages=(
     "libspa-0.2-modules"
@@ -19,9 +19,6 @@ hotfix() {
     "pipewire-v4l2"
     "gstreamer1.0-pipewire"
   )
-  pacdeps=("libwebrtc-audio-processing1-deb")
-
-  pacstall -PI "${pacdeps[@]}"
 
   for i in "${!packages[@]}"; do
     wget -q "${base_url}/${packages[i]}_${pkgver}_${CARCH}.deb" -O "${i}_${name}.deb"
