b161263b9b34277e2f5d0330064b0dcea57ae875
ook37 <oren+12345@taumoda.com>
2025-07-08 20:11:48 -0400

    bump

--- scripts/aacurl.sh
+++ scripts/aacurl.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 hotfix() {
-  sudo apt update && sudo apt install apparmor-utils -y
+  { sudo apt update || :; }
+  sudo apt install apparmor-utils -y
   sudo aa-disable curl
 }
