b25e6917a06dc27c35dd8e2261211418e6cd633d
oklopfer <104327997+oklopfer@users.noreply.github.com>
2025-12-15 18:09:32 -0500

    fix

--- scripts/active/nix-bin.sh
+++ scripts/active/nix-bin.sh
@@ -2,13 +2,13 @@
 
 hotfix() {
   pkgname="nix-bin"
-  badver="2.26.3+dfsg-1ubuntu2"
+  badver="2.26.3+dfsg-1ubuntu"
   goodver="2.26.3+dfsg-1+b1"
   CARCH="$(dpkg --print-architecture)"
 
   mapfile -t status < <(dpkg-query -W --showformat='${db:Status-Status}\n${Version}' "${pkgname}" 2> /dev/null)
   if [[ ${status[0]} == "installed" ]]; then
-    if [[ ${status[1]} == "${badver}" ]]; then
+    if [[ ${status[1]} == "${badver}"* ]]; then
       wget -q "http://ftp.debian.org/debian/pool/main/n/nix/${pkgname}_${goodver}_${CARCH}.deb"
       sudo apt install -f ./"${pkgname}_${goodver}_${CARCH}.deb" -y
     else
